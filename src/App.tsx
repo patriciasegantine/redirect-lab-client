34b4d2aa917b76478e513ec568523c147af97090
Patricia Segantine <pnsegantine@gmail.com>
2026-03-17 22:11:48 +0000

    feat(routes): define application routes and update App component to use them

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Layout } from "@/components/layout"
 import { Home } from "@/components/pages/home"
 import { NotFound } from "@/components/pages/not-found"
 import { Redirecting } from "@/components/pages/redirecting"
+import { AppRoutes } from "@/lib/routes"
 import { Route, Routes } from "react-router-dom"
 
 export function App() {
@@ -12,9 +13,9 @@ export function App() {
       
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/test-redirect" element={<Redirecting />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path={AppRoutes.HOME} element={<Home />} />
+          <Route path={AppRoutes.REDIRECT} element={<Redirecting />} />
+          <Route path={AppRoutes.NOT_FOUND} element={<NotFound />} />
         </Routes>
       </Layout>
     </div>
